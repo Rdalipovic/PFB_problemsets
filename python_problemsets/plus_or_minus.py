65dd79a0139f7c1cdcd4f9acc1aeb9b25098b518
pfb18 <pfb18@info18.cshl.edu>
2023-10-17 11:31:56 -0400

    fixed naming errors and added plus_minus.py

---
+++ python_problemsets/plus_or_minus.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+
+number_arg = int(sys.argv[1])
+
+if number_arg > 0:
+   print("positive")
+elif number_arg = 0:
+   print("Zero")
+else:
+   print("negative")
+
--- python_problemsets/python2_script.py
+++ python_problemsets/python2_script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-_the_deep_sea = "Arrr Captain!"
+the_deep_sea = "Arrr Captain!"
 if bool(_the_deep_sea) == True:
    print("Yarr Matey!")
 else:
