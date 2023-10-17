845986daae1c9c3b22493e35a1e93d6f411e4e54
pfb18 <pfb18@info18.cshl.edu>
2023-10-17 11:02:14 -0400

    played around with f"" string formating and sys module for sys.argv function use

--- test_script.py
+++ test_script.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
-print('Robert')
+import sys
+
+name = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+
+print(f'My name: {name}')
+print(f"My favourite color: {color}")
+print(f"My favourite activity: {activity}")
+print(f"My favourite animal: {animal}")
+
