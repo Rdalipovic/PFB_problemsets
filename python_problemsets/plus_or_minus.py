52b9e545723da06a8f27216cf78f6c363d504b98
pfb18 <pfb18@info18.cshl.edu>
2023-10-17 11:53:40 -0400

    added extra if else statements

--- python_problemsets/plus_or_minus.py
+++ python_problemsets/plus_or_minus.py
@@ -2,10 +2,18 @@
 
 import sys
 
-number_arg = int(sys.argv[1])
+number_arg = float(sys.argv[1])
 
 if number_arg > 0:
    print("positive")
+   if number_arg =< 50:
+      print("Number also equal or smaller than 50")
+      if (number_arg % 2) == 0:
+         print("it is an even number that is smaller than 50")
+   else:
+      print("Number is larger than 50")
+      if number_arg % 3 == 0:
+         print("Number is larger than 50 and divisible by 3")         
 elif number_arg == 0:
    print("Zero")
 else:
